aad73503f299e5454375c7f82a90d62cf9650272
sthaamula <sthaamulya@gmail.com>
2019-06-18 14:04:42 +0545

    Add: Added learner side functionality.

--- CP Project/Implementation/Interactive-Tutorial-Platform/src/app/app-routing.module.ts
+++ CP Project/Implementation/Interactive-Tutorial-Platform/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'learner', loadChildren: () => import('./feature/learner/learner.module').then(m => m.LearnerModule)},
+  {path: 'authentication', loadChildren: () => import('./feature/authentication/authentication.module').then(m => m.AuthenticationModule)},
+  {path: 'admin', loadChildren: () => import('./feature/admin/admin.module').then(m => m.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
